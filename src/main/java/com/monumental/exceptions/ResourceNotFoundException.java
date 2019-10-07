64a81de0d00d220988d1f02a8a90db23deb65e95
Nicholas Deyette <nicholas.deyette@cloudcheckr.com>
2019-10-07 16:51:20 -0400

    Renamed NotFoundException to ResourceNotFoundException

--- src/main/java/com/monumental/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/monumental/exceptions/ResourceNotFoundException.java
@@ -7,14 +7,14 @@ import org.springframework.http.HttpStatus;
  * Exception class used for HTTP Not Found statuses (404)
  * Extends the RuntimeException class
  */
-@ExceptionMapping(statusCode = HttpStatus.NOT_FOUND, errorCode = "notFound")
-public class NotFoundException extends RuntimeException {
+@ExceptionMapping(statusCode = HttpStatus.NOT_FOUND, errorCode = "The requested resource could not be found")
+public class ResourceNotFoundException extends RuntimeException {
 
     /**
      * Public constructor for a new NotFoundException
      * @param message - Message for the exception, as a String
      */
-    public NotFoundException(String message) {
+    public ResourceNotFoundException(String message) {
         super(message);
     }
 }
