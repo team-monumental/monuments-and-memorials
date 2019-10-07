36542e3aa831fbb5698ab025824b1458348ca8cb
benvogler <bpv1520@rit.edu>
2019-10-07 13:27:05 -0400

    Change logo and log in/sign up buttons

--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -5,13 +5,7 @@ export default class Logo extends React.Component {
     render() {
         return (
             <div className="logo">
-                <div className="M">
-                    M
-                </div>
-                <div className="col">
-                    <span>emorials<span style={{fontSize: '0.5em'}}>&nbsp;</span>&</span>
-                    <span>onuments</span>
-                </div>
+                M
             </div>
         );
     }
