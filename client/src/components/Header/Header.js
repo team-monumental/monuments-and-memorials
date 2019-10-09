01a7c1d3ff42fa3f13fe4e2bc718122ebc65eb73
Ben Vogler <bpv1520@rit.edu>
2019-10-09 19:42:42 -0400

    Dev -> Master (#18)
    * Initial commit of branch for unit testing Service CRUD
    
    * Started mocking for unit tests, wrote first unit test
    
    * Finished implementing MonumentService unit tests
    
    * Add deployment documentation
    
    * Fix typo
    
    * Fix gradle test execution
    
    * Initial commit for adding API 404 errors to /api/monument/{id}
    
    * React router setup and basic view monument page
    
    * Change logo and log in/sign up buttons
    
    * Fix key error
    
    * Renamed NotFoundException to ResourceNotFoundException
    
    * Change run script to use port 80 (#15)
    
    * Implemented a custom error message for attempting to retrieve a
    non-existent Monument record
    
    * Added comments
    
    * Made error code message and Exception class generic
    
    * Fixed the order of latitude and longitude, Removed quotes from fields
    
    * Added removeBeginningAndEndingQuotes method

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './Header.css';
 import Logo from '../Logo/Logo';
+import { Link } from 'react-router-dom';
 
 export default class Header extends React.Component {
+
+    links = [
+        {name: 'Home', route: '/'},
+        {name: 'About', route: '/about'},
+        {name: 'Contribute', route: '/contribute'},
+        {name: 'Map', route: '/map'}
+    ];
 
     render() {
         return (
@@ -10,11 +18,12 @@ export default class Header extends React.Component {
                 <div className="left">
                     <Logo/>
                 </div>
+                <div className="center">
+                    {this.links.map(link =>
+                        <Link to={link.route} className="header-link" key={link.name}>{link.name}</Link>
+                    )}
+                </div>
                 <div className="right">
-                    <a className="header-link" href="/">Home</a>
-                    <a className="header-link" href="/about">About</a>
-                    <a className="header-link" href="/contribute">Contribute</a>
-                    <a className="header-link active" href="/map">Map</a>
                     <button className="button-outline">Log in</button>
                     <button className="button">Sign up</button>
                 </div>
