8a12c28ec75d6d924660fadd4bb8b351e10a7de2
Ben Vogler <bpv1520@rit.edu>
2019-10-14 09:03:41 -0400

    Add address lookup via reverse geocoding (#26)
    * Add reverse geocoding support
    
    * Remove debug

--- src/main/java/com/monumental/controllers/MonumentController.java
+++ src/main/java/com/monumental/controllers/MonumentController.java
@@ -15,7 +15,7 @@ public class MonumentController {
     private MonumentService monumentService;
 
     @PostMapping("/api/monument")
-    public Monument createMonument(Monument monument) {
+    public Monument createMonument(@RequestBody Monument monument) {
         this.monumentService.insert(monument);
         return monument;
     }
@@ -37,8 +37,13 @@ public class MonumentController {
     }
 
     @PutMapping("/api/monument/{id}")
-    public Monument updateMonument(@PathVariable("id") Integer id, Monument monument) {
-        this.monumentService.update(monument);
+    public Monument updateMonument(@PathVariable("id") Integer id, @RequestBody Monument monument) {
+        monument.setId(id);
+        try {
+            this.monumentService.update(monument);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return monument;
     }
 }
