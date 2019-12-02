c9380c24a48e0e21753554a4f9957d1e970d96d3
Nicholas Deyette <njd9419@rit.edu>
2019-12-01 23:18:46 -0500

    Refactored Address Component (#63)
    * Fixed issues with showing location symbol on Monument Page
    Refactored SearchResult component to use Address component
    
    * Removed duplicate code
    
    * Remove space from className

--- client/src/components/Monument/Details/Address/Address.js
+++ client/src/components/Monument/Details/Address/Address.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Address.scss';
 
 /**
  * Renders the Monument's address or city, state if there's no address
@@ -7,14 +8,28 @@ export default class Address extends React.Component {
 
     render() {
         const monument = this.props.monument;
+        const locationIcon = (
+            <i className='material-icons'>room</i>
+        );
+
         if (monument.address) {
             return (
-                <div style={{display: 'flex', alignItems: 'center'}}>
-                    <i className="material-icons">room</i> {monument.address}
+                <div style={{display: 'flex', alignItems: 'center'}} className='address-container font-italic'>
+                    {locationIcon} {monument.address}
                 </div>
             )
-        } else return (
-            <div>{[monument.city, monument.state].filter(str => str && str.trim()).join(', ')}</div>
-        );
+        } else if (monument.city && monument.state) {
+            return (
+                <div className='address-container font-italic'>
+                    {locationIcon}
+                    <span className='city-state'>{[monument.city, monument.state].filter(str => str && str.trim()).join(', ')}</span>
+                </div>
+            );
+        }
+        else {
+            return (
+                <div/>
+            );
+        }
     }
 }
