d500dff0d1f3a434dcbdf72b8c0be505731e9097
= <njd9419@rit.edu>
2019-12-19 08:08:32 -0500

    Completed implementation of Tag Directory Page

--- client/src/pages/TagDirectoryPage/TagDirectoryPage.js
+++ client/src/pages/TagDirectoryPage/TagDirectoryPage.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './TagDirectoryPage.scss';
+import fetchTags from '../../actions/tagDirectory';
+import { connect } from 'react-redux';
+import Spinner from '../../components/Spinner/Spinner';
+import TagColumn from '../../components/TagColumn/TagColumn';
 
 /**
  * Root container component for the Tag Directory Page
@@ -10,19 +14,54 @@ class TagDirectoryPage extends React.Component {
         super(props);
 
         const { dispatch } = this.props;
-        // TODO: Dispatch Fetch Tags
+
+        dispatch(fetchTags());
     }
 
     static mapStateToProps(state) {
-         // TODO
+        return state.tagDirectoryPage;
     }
 
     render() {
-        const { tags } = this.props;
+        const { tags, fetchTagsPending } = this.props;
+
+        const allTags = [];
+        const allMaterials = [];
+
+        tags.forEach((tag) => {
+            if (tag.isMaterial) {
+                allMaterials.push(tag);
+            }
+            else {
+                allTags.push(tag);
+            }
+        });
 
         return (
             <div className='tag-directory-page-container'>
+                <Spinner show={fetchTagsPending}/>
+                <div className='page-title'>
+                    <h1>
+                        Tag Directory
+                    </h1>
+                </div>
+                <div className='columns-container'>
+                    <div className='tags-column column'>
+                        <TagColumn
+                            variant='tags'
+                            tags={allTags}
+                        />
+                    </div>
+                    <div className='materials-column column'>
+                        <TagColumn
+                            variant='materials'
+                            tags={allMaterials}
+                        />
+                    </div>
+                </div>
             </div>
         );
     }
 }
+
+export default connect(TagDirectoryPage.mapStateToProps)(TagDirectoryPage);
--- client/src/reducers/tagDirectory.js
+++ client/src/reducers/tagDirectory.js
@@ -6,3 +6,26 @@ const initialState = {
     error: null
 };
 
+export default function tagDirectoryPage(state = initialState, action) {
+    switch (action.type) {
+        case FETCH_TAGS_PENDING:
+            return {
+                ...state,
+                fetchTagsPending: true
+            };
+        case FETCH_TAGS_SUCCESS:
+            return {
+                ...state,
+                fetchTagsPending: false,
+                tags: action.payload
+            };
+        case FETCH_TAGS_ERROR:
+            return {
+                ...state,
+                fetchTagsPending: false,
+                error: action.error
+            };
+        default:
+            return state;
+    }
+}
