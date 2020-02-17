1692674577bb916b6eb71559fa028769811d4f52
benvogler <bpv1520@rit.edu>
2020-02-17 18:30:14 -0500

    Update renew script

--- renew-ssl.sh
+++ renew-ssl.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-sudo certbot renew --dry-run
+now=$(date)
+echo "===(${now}) Renewing SSL Certificate ==="
+
+sudo certbot renew --non-interactive --force-renewal
 sudo openssl pkcs12 -export -in /etc/letsencrypt/live/monuments.us.org/fullchain.pem -inkey /etc/letsencrypt/live/monuments.us.org/privkey.pem -out /etc/letsencrypt/live/monuments.us.org/keystore.p12 -name tomcat -CAfile /etc/letsencrypt/live/monuments.us.org/chain.pem -caname root
 cd /home/ubuntu/monuments-and-memorials
 sudo cp /etc/letsencrypt/live/monuments.us.org/keystore.p12 ./
 ./gradlew build -s -x test
 sudo systemctl restart monumental
+
+echo "===(${now}) Renewed SSL Certificate ==="
