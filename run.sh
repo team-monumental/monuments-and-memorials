3201b5d3047c9e57b3f7550659cac5efe51009ab
benvogler <bpv1520@rit.edu>
2020-02-17 16:47:24 -0500

    Update run script to use SSL

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar build/libs/monuments-and-memorials-0.0.1-SNAPSHOT.jar --server.port=8080
+java -jar build/libs/monuments-and-memorials-0.0.1-SNAPSHOT.jar --server.port=8443 --security.require-ssl=true --server.ssl.key-store=${SSL_KEY_STORE} --server.ssl.key-store-password=${SSL_PASSWORD} --server.ssl.keyStoreType=PKCS12 --server.ssl.keyAlias=tomcat
