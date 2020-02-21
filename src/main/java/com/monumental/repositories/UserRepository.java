996c62ccf7a235ddf50fff854059bfd37535add6
Ben Vogler <bpv1520@rit.edu>
2020-02-20 21:39:11 -0500

    Fix repositories

--- src/main/java/com/monumental/repositories/UserRepository.java
+++ src/main/java/com/monumental/repositories/UserRepository.java
@@ -2,7 +2,12 @@ package com.monumental.repositories;
 
 import com.monumental.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     User getByEmail(String email);
--- src/main/java/com/monumental/repositories/VerificationTokenRepository.java
+++ src/main/java/com/monumental/repositories/VerificationTokenRepository.java
@@ -3,9 +3,13 @@ package com.monumental.repositories;
 import com.monumental.models.User;
 import com.monumental.models.VerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Repository
+@Transactional
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Integer> {
 
     VerificationToken getByToken(String token);
