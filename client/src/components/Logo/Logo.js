4a39700a5c12bba3d35073808b29ae4bbc4c2822
Ben Vogler <bpv1520@rit.edu>
2020-02-22 10:53:47 -0500

    Fix alternate logo sizes, update Logo component to use them intelligently

--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -4,9 +4,13 @@ import './Logo.scss';
 export default class Logo extends React.Component {
     render() {
         const { size, className } = this.props;
+        let pixelSize = parseInt(size.replace('px', ''));
+        let logoSize = 192;
+        if (pixelSize > 192 && pixelSize <= 512) logoSize = 512;
+        if (pixelSize > 512) logoSize = '-full-size';
         return (
             <img className={['logo', className].join(' ')}
-                 src={process.env.PUBLIC_URL + '/MM-logo-rev-3.png'}
+                 src={`${process.env.PUBLIC_URL}/logo${logoSize}.png`}
                  alt="Monuments and Memorials Logo"
                  width={size}
                  height={size}/>
