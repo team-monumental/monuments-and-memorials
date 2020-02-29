79a0adbdbaa36b0b577e202ff81462c117c293c7
= <njd9419@rit.edu>
2020-02-29 15:08:45 -0500

    Implemented endpoints for CreateMonumentSuggestions

--- src/main/java/com/monumental/controllers/SuggestionController.java
+++ src/main/java/com/monumental/controllers/SuggestionController.java
@@ -1,16 +1,12 @@
 package com.monumental.controllers;
 
 import com.monumental.exceptions.ResourceNotFoundException;
-import com.monumental.models.Monument;
 import com.monumental.models.suggestions.CreateMonumentSuggestion;
 import com.monumental.repositories.suggestions.CreateSuggestionRepository;
 import com.monumental.security.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
 import java.util.Map;
@@ -22,7 +18,12 @@ public class SuggestionController {
     @Autowired
     private CreateSuggestionRepository createSuggestionRepository;
 
-    @PostMapping("/api/suggest/create")
+    /**
+     * Create a new Suggestion for creating a Monument
+     * @param createSuggestion - CreateMonumentSuggestion object representing the new Monument to suggest
+     * @return Map<String, Boolean> - Map of result String to actual result
+     */
+    @PostMapping("/api/suggestion/create")
     @PreAuthorize(Authentication.isAuthenticated)
     @Transactional
     public Map<String, Boolean> suggestMonumentCreation(@RequestBody CreateMonumentSuggestion createSuggestion) {
@@ -30,13 +31,71 @@ public class SuggestionController {
         return Map.of("success", true);
     }
 
-    @PostMapping("/api/suggest/create/approve/{id}")
+    /**
+     * Get a CreateMonumentSuggestion with the specified ID, if it exists
+     * @param id - ID of the CreateMonumentSuggestion to get
+     * @return CreateMonumentSuggestion - CreateMonumentSuggestion object with the specified ID, if it exists
+     * @throws ResourceNotFoundException - If a CreateMonumentSuggestion with the specified ID does not exist
+     */
+    @GetMapping("/api/suggestion/create/{id}")
+    // TODO
+    //@PreAuthorize()
+    @Transactional
+    public CreateMonumentSuggestion getCreateMonumentSuggestion(@PathVariable("id") Integer id)
+            throws ResourceNotFoundException {
+        return this.findCreateSuggestion(id);
+    }
+
+    /**
+     * Approve a CreateMonumentSuggestion with the specified ID, if it exists
+     * @param id - ID of the CreateMonumentSuggestion to approve
+     * @return Map<String, Boolean> - Map of result String to actual result
+     * @throws ResourceNotFoundException - If a CreateMonumentSuggestion with the specified ID does not exist
+     */
+    @PostMapping("/api/suggestion/create/{id}/approve")
+    // TODO
     //@PreAuthorize(Authorization.)
     @Transactional
-    public Monument approveCreateSuggestion(@PathVariable("id") Integer id) {
+    public Map<String, Boolean> approveCreateSuggestion(@PathVariable("id") Integer id)
+            throws ResourceNotFoundException {
+        CreateMonumentSuggestion createSuggestion = this.findCreateSuggestion(id);
+
+        createSuggestion.setIsApproved(true);
+        this.createSuggestionRepository.save(createSuggestion);
+
+        return Map.of("success", true);
+    }
+
+    /**
+     * Reject a CreateMonumentSuggestion with the specified ID, if it exists
+     * @param id - ID of the CreateMonumentSuggestion to reject
+     * @return Map<String, Boolean> - Map of result String to actual result
+     * @throws ResourceNotFoundException - If a CreateMonumentSuggestion with the specified ID does not exist
+     */
+    @PostMapping("/api/suggestion/create/{id}/reject")
+    // TODO
+    //@PreAuthorize()
+    @Transactional
+    public Map<String, Boolean> rejectCreateSuggestion(@PathVariable("id") Integer id) throws ResourceNotFoundException {
+        CreateMonumentSuggestion createSuggestion = this.findCreateSuggestion(id);
+
+        createSuggestion.setIsRejected(true);
+        this.createSuggestionRepository.save(createSuggestion);
+
+        return Map.of("success", true);
+    }
+
+    /**
+     * Helper function to find a CreateMonumentSuggestion object with the specified ID, if it exists
+     * @param id - ID of the CreateMonumentSuggestion object to find
+     * @return CreateMonumentSuggestion - CreateMonumentSuggestion object with the specified ID, if it exists
+     * @throws ResourceNotFoundException - If a CreateMonumentSuggestion does not exist with the specified ID
+     */
+    private CreateMonumentSuggestion findCreateSuggestion(Integer id) throws ResourceNotFoundException {
         Optional<CreateMonumentSuggestion> optional = this.createSuggestionRepository.findById(id);
         if (optional.isEmpty()) {
             throw new ResourceNotFoundException("The requested Suggestion does not exist");
         }
+        return optional.get();
     }
 }
