e3bee807e9e5013eab894add8cd015112fab8175
Ben Vogler <bpv1520@rit.edu>
2020-03-01 12:03:16 -0500

    Update ssl renewal script (#108)

--- renew-ssl.sh
+++ renew-ssl.sh
@@ -3,11 +3,8 @@
 now=$(date)
 echo "===(${now}) Renewing SSL Certificate ==="
 
-sudo certbot renew --non-interactive --force-renewal
-sudo openssl pkcs12 -export -in /etc/letsencrypt/live/monuments.us.org/fullchain.pem -inkey /etc/letsencrypt/live/monuments.us.org/privkey.pem -out /etc/letsencrypt/live/monuments.us.org/keystore.p12 -name tomcat -CAfile /etc/letsencrypt/live/monuments.us.org/chain.pem -caname root
-cd /home/ubuntu/monuments-and-memorials
-sudo cp /etc/letsencrypt/live/monuments.us.org/keystore.p12 ./
-./gradlew build -s -x test
-sudo systemctl restart monumental
-
-echo "===(${now}) Renewed SSL Certificate ==="
+sudo certbot renew --non-interactive --post-hook "
+sudo openssl pkcs12 -export -in /etc/letsencrypt/live/monuments.us.org/fullchain.pem -inkey /etc/letsencrypt/live/monuments.us.org/privkey.pem -out /etc/letsencrypt/live/monuments.us.org/keystore.p12 -name tomcat -CAfile /etc/letsencrypt/live/monuments.us.org/chain.pem -caname root --password pass: && \
+cd /home/ubuntu/monuments-and-memorials && sudo cp /etc/letsencrypt/live/monuments.us.org/keystore.p12 ./ && \
+./gradlew build -s -x test && \
+sudo systemctl restart monumental"
