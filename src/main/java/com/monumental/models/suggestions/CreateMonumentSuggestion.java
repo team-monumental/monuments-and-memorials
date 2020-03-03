7288e0d24ca5a74663b34252ca0c7de462b78ad5
= <njd9419@rit.edu>
2020-03-03 17:21:28 -0500

    Fix Suggestion Models

--- src/main/java/com/monumental/models/suggestions/CreateMonumentSuggestion.java
+++ src/main/java/com/monumental/models/suggestions/CreateMonumentSuggestion.java
@@ -3,6 +3,7 @@ package com.monumental.models.suggestions;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 /**
  * Class used to represent a suggestion to create a new Monument record
@@ -10,8 +11,57 @@ import javax.persistence.*;
 @Entity
 public class CreateMonumentSuggestion extends MonumentSuggestion {
 
-    @Column(name = "json")
-    private String json;
+    @Column(name = "title")
+    @NotNull(groups = {New.class, Existing.class}, message = "Title can not be null")
+    private String title;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "latitude")
+    private Double latitude;
+
+    @Column(name = "longitude")
+    private Double longitude;
+
+    @Column(name = "year")
+    private String year;
+
+    @Column(name = "month")
+    private String month;
+
+    @Column(name = "date")
+    private String date;
+
+    @Column(name = "artist")
+    private String artist;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "inscription")
+    private String inscription;
+
+    @Column(name = "is_temporary")
+    private boolean isTemporary;
+
+    @Column(name = "references_json")
+    private String referencesJson;
+
+    @Column(name = "materials_json")
+    private String materialsJson;
+
+    @Column(name = "new_materials_json")
+    private String newMaterialsJson;
+
+    @Column(name = "tags_json")
+    private String tagsJson;
+
+    @Column(name = "new_tags_json")
+    private String newTagsJson;
+
+    @Column(name = "images_json")
+    private String imagesJson;
 
     @JsonIgnore
     @ManyToOne
@@ -22,11 +72,147 @@ public class CreateMonumentSuggestion extends MonumentSuggestion {
 
     }
 
-    public String getJson() {
-        return this.json;
+    public String getTitle() {
+        return this.title;
     }
 
-    public void setJson(String json) {
-        this.json = json;
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Double getLatitude() {
+        return this.latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return this.longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getYear() {
+        return this.year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getMonth() {
+        return this.month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getArtist() {
+        return this.artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getInscription() {
+        return this.inscription;
+    }
+
+    public void setInscription(String inscription) {
+        this.inscription = inscription;
+    }
+
+    public boolean getIsTemporary() {
+        return this.isTemporary;
+    }
+
+    public void setIsTemporary(boolean isTemporary) {
+        this.isTemporary = isTemporary;
+    }
+
+    public String getReferencesJson() {
+        return this.referencesJson;
+    }
+
+    public void setReferencesJson(String referencesJson) {
+        this.referencesJson = referencesJson;
+    }
+
+    public String getMaterialsJson() {
+        return this.materialsJson;
+    }
+
+    public void setMaterialsJson(String materialsJson) {
+        this.materialsJson = materialsJson;
+    }
+
+    public String getNewMaterialsJson() {
+        return this.newMaterialsJson;
+    }
+
+    public void setNewMaterialsJson(String newMaterialsJson) {
+        this.newMaterialsJson = newMaterialsJson;
+    }
+
+    public String getTagsJson() {
+        return this.tagsJson;
+    }
+
+    public void setTagsJson(String tagsJson) {
+        this.tagsJson = tagsJson;
+    }
+
+    public String getNewTagsJson() {
+        return this.newTagsJson;
+    }
+
+    public void setNewTagsJson(String newTagsJson) {
+        this.newTagsJson = newTagsJson;
+    }
+
+    public String getImagesJson() {
+        return this.imagesJson;
+    }
+
+    public void setImagesJson(String imagesJson) {
+        this.imagesJson = imagesJson;
+    }
+
+    public BulkCreateMonumentSuggestion getBulkCreateSuggestion() {
+        return this.bulkCreateSuggestion;
+    }
+
+    public void setBulkCreateSuggestion(BulkCreateMonumentSuggestion bulkCreateSuggestion) {
+        this.bulkCreateSuggestion = bulkCreateSuggestion;
     }
 }
--- src/main/java/com/monumental/models/suggestions/UpdateMonumentSuggestion.java
+++ src/main/java/com/monumental/models/suggestions/UpdateMonumentSuggestion.java
@@ -3,6 +3,7 @@ package com.monumental.models.suggestions;
 import com.monumental.models.Monument;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 /**
  * Class used to represent a suggestion to update an existing Monument record
@@ -14,8 +15,63 @@ public class UpdateMonumentSuggestion extends MonumentSuggestion {
     @JoinColumn(name = "monument_id", nullable = false)
     private Monument monument;
 
-    @Column(name = "json")
-    private String json;
+    @Column(name = "new_title")
+    @NotNull(groups = {New.class, Existing.class}, message = "New title can not be null")
+    private String newTitle;
+
+    @Column(name = "new_address")
+    private String newAddress;
+
+    @Column(name = "new_artist")
+    private String newArtist;
+
+    @Column(name = "new_description")
+    private String newDescription;
+
+    @Column(name = "new_inscription")
+    private String newInscription;
+
+    @Column(name = "new_latitude")
+    private Double newLatitude;
+
+    @Column(name = "new_longitude")
+    private Double newLongitude;
+
+    @Column(name = "new_year")
+    private String newYear;
+
+    @Column(name = "new_month")
+    private String newMonth;
+
+    @Column(name = "new_date")
+    private String newDate;
+
+    @Column(name = "new_is_temporary")
+    private boolean newIsTemporary;
+
+    @Column(name = "updated_reference_urls_by_id_json")
+    private String updatedReferenceUrlsByIdJson;
+
+    @Column(name = "new_reference_urls_json")
+    private String newReferenceUrlsJson;
+
+    @Column(name = "deleted_reference_ids_json")
+    private String deletedReferenceIdsJson;
+
+    @Column(name = "new_image_urls_json")
+    private String newImageUrlsJson;
+
+    @Column(name = "new_primary_image_id")
+    private Integer newPrimaryImageId;
+
+    @Column(name = "deleted_image_ids_json")
+    private String deletedImageIdsJson;
+
+    @Column(name = "new_materials_json")
+    private String newMaterialsJson;
+
+    @Column(name = "new_tags_json")
+    private String newTagsJson;
 
     public UpdateMonumentSuggestion() {
 
@@ -29,11 +85,155 @@ public class UpdateMonumentSuggestion extends MonumentSuggestion {
         this.monument = monument;
     }
 
-    public String getJson() {
-        return this.json;
+    public String getNewTitle() {
+        return this.newTitle;
     }
 
-    public void setJson(String json) {
-        this.json = json;
+    public void setNewTitle(String newTitle) {
+        this.newTitle = newTitle;
+    }
+
+    public String getNewAddress() {
+        return this.newAddress;
+    }
+
+    public void setNewAddress(String newAddress) {
+        this.newAddress = newAddress;
+    }
+
+    public String getNewArtist() {
+        return this.newArtist;
+    }
+
+    public void setNewArtist(String newArtist) {
+        this.newArtist = newArtist;
+    }
+
+    public String getNewDescription() {
+        return this.newDescription;
+    }
+
+    public void setNewDescription(String newDescription) {
+        this.newDescription = newDescription;
+    }
+
+    public String getNewInscription() {
+        return this.newInscription;
+    }
+
+    public void setNewInscription(String newInscription) {
+        this.newInscription = newInscription;
+    }
+
+    public Double getNewLatitude() {
+        return this.newLatitude;
+    }
+
+    public void setNewLatitude(Double newLatitude) {
+        this.newLatitude = newLatitude;
+    }
+
+    public Double getNewLongitude() {
+        return this.newLongitude;
+    }
+
+    public void setNewLongitude(Double newLongitude) {
+        this.newLongitude = newLongitude;
+    }
+
+    public String getNewYear() {
+        return this.newYear;
+    }
+
+    public void setNewYear(String newYear) {
+        this.newYear = newYear;
+    }
+
+    public String getNewMonth() {
+        return this.newMonth;
+    }
+
+    public void setNewMonth(String newMonth) {
+        this.newMonth = newMonth;
+    }
+
+    public String getNewDate() {
+        return this.newDate;
+    }
+
+    public void setNewDate(String newDate) {
+        this.newDate = newDate;
+    }
+
+    public boolean getNewIsTemporary() {
+        return this.newIsTemporary;
+    }
+
+    public void setNewIsTemporary(boolean temporary) {
+        this.newIsTemporary = temporary;
+    }
+
+    public String getUpdatedReferenceUrlsByIdJson() {
+        return this.updatedReferenceUrlsByIdJson;
+    }
+
+    public void setUpdatedReferenceUrlsByIdJson(String updatedReferenceUrlsByIdJson) {
+        this.updatedReferenceUrlsByIdJson = updatedReferenceUrlsByIdJson;
+    }
+
+    public String getNewReferenceUrlsJson() {
+        return this.newReferenceUrlsJson;
+    }
+
+    public void setNewReferenceUrlsJson(String newReferenceUrlsJson) {
+        this.newReferenceUrlsJson = newReferenceUrlsJson;
+    }
+
+    public String getDeletedReferenceIdsJson() {
+        return this.deletedReferenceIdsJson;
+    }
+
+    public void setDeletedReferenceIdsJson(String deletedReferenceIdsJson) {
+        this.deletedReferenceIdsJson = deletedReferenceIdsJson;
+    }
+
+    public String getNewImageUrlsJson() {
+        return this.newImageUrlsJson;
+    }
+
+    public void setNewImageUrlsJson(String newImageUrlsJson) {
+        this.newImageUrlsJson = newImageUrlsJson;
+    }
+
+    public Integer getNewPrimaryImageId() {
+        return this.newPrimaryImageId;
+    }
+
+    public void setNewPrimaryImageId(Integer newPrimaryImageId) {
+        this.newPrimaryImageId = newPrimaryImageId;
+    }
+
+    public String getDeletedImageIdsJson() {
+        return this.deletedImageIdsJson;
+    }
+
+    public void setDeletedImageIdsJson(String deletedImageIdsJson) {
+        this.deletedImageIdsJson = deletedImageIdsJson;
+    }
+
+    public String getNewMaterialsJson() {
+        return this.newMaterialsJson;
+    }
+
+    public void setNewMaterialsJson(String newMaterialsJson) {
+        this.newMaterialsJson = newMaterialsJson;
+    }
+
+    public String getNewTagsJson() {
+        return this.newTagsJson;
+    }
+
+    public void setNewTagsJson(String newTagsJson) {
+        this.newTagsJson = newTagsJson;
     }
 }
