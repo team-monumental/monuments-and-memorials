b7963016796b5ed8f70c6662d17019387f427ccd
Ben Vogler <bpv1520@rit.edu>
2020-03-11 01:23:57 -0400

    Add pagination to user search

--- client/src/components/AdminPanel/ManageUsers/UserSearch/UserSearch.js
+++ client/src/components/AdminPanel/ManageUsers/UserSearch/UserSearch.js
@@ -1,18 +1,34 @@
 import * as React from 'react';
 import UserSearchBar from './UserSearchBar/UserSearchBar';
 import UserSearchResults from './UserSearchResults/UserSearchResults';
+import Pagination from '../../../Pagination/Pagination';
+import SearchInfo from '../../../Search/SearchInfo/SearchInfo';
 
 export default class UserSearch extends React.Component {
 
     render() {
-        const { showSearchResults, onLimitChange, limit, page, count, users } = this.props;
+        const { showSearchResults, onLimitChange, onPageChange, limit, page, count, users } = this.props;
+
+        const pageCount = Math.ceil(count / limit);
 
         return (
             <div className="user-search">
-                <UserSearchBar/>
-                {showSearchResults &&
-                    <UserSearchResults onLimitChange={onLimitChange} limit={limit} page={page} count={count} users={users}/>
-                }
+                <div className="sticky-top">
+                    <UserSearchBar/>
+                    {showSearchResults &&
+                        <div className="mt-2">
+                            <SearchInfo onLimitChange={onLimitChange} limit={limit} page={page} count={count} hideSortBy/>
+                        </div>
+                    }
+                </div>
+                {showSearchResults && <>
+                    <UserSearchResults limit={limit} page={page} users={users}/>
+                    <div className="pagination-container">
+                        <Pagination count={pageCount}
+                                    page={page - 1}
+                                    onPage={page => onPageChange(page + 1)}/>
+                    </div>
+                </>}
             </div>
         );
     }
--- client/src/components/AdminPanel/ManageUsers/UserSearch/UserSearchResults/UserSearchResults.js
+++ client/src/components/AdminPanel/ManageUsers/UserSearch/UserSearchResults/UserSearchResults.js
@@ -1,18 +1,16 @@
 import * as React from 'react';
-import SearchInfo from '../../../../Search/SearchInfo/SearchInfo';
 import UserSearchResult from './UserSearchResult/UserSearchResult';
 
 export default class UserSearchResults extends React.Component {
 
     render() {
-        const { onLimitChange, limit, page, count, users } = this.props;
+        const { limit, page, users } = this.props;
         if (users && users.length > 0) {
             return (
                 <div className="user-search-results">
-                    <SearchInfo onLimitChange={onLimitChange} limit={limit} page={page} count={count} hideSortBy/>
                     {
                         users.map((user, index) => (
-                            <UserSearchResult key={user.id} user={user} index={index}/>
+                            <UserSearchResult key={user.id} user={user} index={index + ((page - 1) * limit)}/>
                         ))
                     }
                 </div>
