922fbc450870242f5d0781467b1cdb4427844b62
Ben Vogler <bpv1520@rit.edu>
2020-04-13 15:48:20 -0400

    Basic SEO Sitemap (#122)
    * Basic Sitemap
    
    * Remove /create

---
+++ src/main/java/com/monumental/controllers/SitemapController.java
@@ -0,0 +1,50 @@
+package com.monumental.controllers;
+
+import com.monumental.models.Monument;
+import com.monumental.repositories.MonumentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.redfin.sitemapgenerator.WebSitemapGenerator;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Writer;
+
+import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
+
+@RestController
+public class SitemapController {
+
+    @Value("${PUBLIC_URL:http://localhost:3000}")
+    private String publicUrl;
+
+    @Autowired
+    private MonumentRepository monumentRepository;
+
+    /**
+     * Generates an XML Sitemap for Google to index the site
+     */
+    @RequestMapping(path = "/sitemap.xml", produces = APPLICATION_XML_VALUE)
+    public void getSitemap(HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_XML_VALUE);
+
+        WebSitemapGenerator sitemap = new WebSitemapGenerator(this.publicUrl);
+
+        sitemap.addUrls(
+            this.publicUrl + "/",
+            this.publicUrl + "/about",
+            this.publicUrl + "/map",
+            this.publicUrl + "/search"
+        );
+
+        for (Monument monument : this.monumentRepository.findAllByIsActive(true)) {
+            sitemap.addUrl(this.publicUrl + "/monuments/" + monument.getId());
+        }
+
+        try (Writer writer = response.getWriter()) {
+            writer.append(String.join("", sitemap.writeAsStrings()));
+        }
+    }
+}
