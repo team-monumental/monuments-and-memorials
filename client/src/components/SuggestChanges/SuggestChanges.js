b0b777bc42cbd51bce6365dcf82e9854ed42f765
Ben Vogler <bpv1520@rit.edu>
2020-04-21 17:51:12 -0400

    Change suggest a change button to camel case

--- client/src/components/SuggestChanges/SuggestChanges.js
+++ client/src/components/SuggestChanges/SuggestChanges.js
@@ -27,12 +27,12 @@ export default class SuggestChanges extends React.Component {
                     {isUpdate && <>
                         <p>We pride ourselves on keeping up to date and accurate information.</p>
                         <p>If you think something on this page is incorrect or outdated, please suggest a change. If your change is approved we'll send you an email to let you know, and you will be listed as a contributor on this page!</p>
-                        <Button variant="primary" onClick={() => this.handleButtonClick()}>SUGGEST A CHANGE</Button>
+                        <Button variant="primary" onClick={() => this.handleButtonClick()}>Suggest A Change</Button>
                     </>}
                     {!isUpdate && <>
                         <p>Contributions help us to expand our database of Monuments and Memorials.</p>
                         <p>Your suggestion will be reviewed by our team and then published for the public to see. Your contribution will be credited on the monument or memorial's page.</p>
-                        <Button variant="primary" onClick={() => this.handleButtonClick()}>SUGGEST NEW</Button>
+                        <Button variant="primary" onClick={() => this.handleButtonClick()}>Suggest New</Button>
                     </>}
                 </Card.Body>
             </Card>
