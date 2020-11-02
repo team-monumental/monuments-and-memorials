801c2023f87fdcc3119ef79fd874f9c84e7da710
Ren Chauret <renaudchauret@gmail.com>
2020-11-02 11:33:41 -0500

    attempt to fix images

--- client/src/pages/CreateMonumentPage/CreateMonumentPage.js
+++ client/src/pages/CreateMonumentPage/CreateMonumentPage.js
@@ -84,7 +84,7 @@ class CreateMonumentPage extends React.Component {
 
         // First, upload the images to the temporary S3 folder and save the URLs in the form
         const imageObjectUrls = await uploadImagesToS3(form.images, true);
-        form.imagesJson = JSON.stringify(imageObjectUrls);
+        form.images = JSON.stringify(imageObjectUrls);
 
         // Next, store the PhotoSphere Image URLs in the form
         form.photoSphereImagesJson = JSON.stringify(form.photoSphereImages.map(photoSphereImage => photoSphereImage.url));
