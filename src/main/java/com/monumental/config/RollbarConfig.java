af2839b3148bebb2f48c2758a9964ab3ae6b985b
Ren Chauret <renaudchauret@gmail.com>
2021-02-08 20:47:55 -0500

    idk

--- src/main/java/com/monumental/config/RollbarConfig.java
+++ src/main/java/com/monumental/config/RollbarConfig.java
@@ -9,9 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-
-@Configuration()
-@EnableWebMvc
+@Configuration
 @ComponentScan({
     "com.monumental.config",
     "com.rollbar.spring"
