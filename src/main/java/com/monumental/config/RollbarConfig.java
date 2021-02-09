bce7cda720bab031578a24bf081c24926cf0c246
Ren Chauret <renaudchauret@gmail.com>
2021-02-08 19:43:25 -0500

    fix backend rollbar

--- src/main/java/com/monumental/config/RollbarConfig.java
+++ src/main/java/com/monumental/config/RollbarConfig.java
@@ -3,27 +3,26 @@ package com.monumental.config;
 import com.rollbar.notifier.Rollbar;
 import com.rollbar.notifier.config.Config;
 import com.rollbar.spring.webmvc.RollbarSpringConfigBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 @Configuration()
 @EnableWebMvc
 @ComponentScan({
-
-// UPDATE TO YOUR PROJECT PACKAGE
-        "com.monumental.config",
-        "com.rollbar.spring"
-
+    "com.monumental.config",
+    "com.rollbar.spring"
 })
 public class RollbarConfig {
 
-    @Autowired
-    private Environment environment;
+    @Value("${ROLLBAR_SERVER_KEY:111}")
+    private String rollbarServerKey;
+
+    @Value("${ROLLBAR_ENV:development}")
+    private String rollbarEnv;
 
     /**
      * Register a Rollbar bean to configure App with Rollbar.
@@ -32,14 +31,14 @@ public class RollbarConfig {
     public Rollbar rollbar() {
 
         // Need to set this environment variable
-        return new Rollbar(getRollbarConfigs(environment.getProperty("REACT_APP_ROLLBAR_SERVER_KEY")));
+        return new Rollbar(getRollbarConfigs(rollbarServerKey));
     }
 
     private Config getRollbarConfigs(String accessToken) {
 
         // Reference ConfigBuilder.java for all the properties you can set for Rollbar
         return RollbarSpringConfigBuilder.withAccessToken(accessToken)
-                .environment("development")
+                .environment(rollbarEnv)
                 .build();
     }
 }
