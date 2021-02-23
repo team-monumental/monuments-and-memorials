fbf1ffd16633fae42d9178d29a1e43cab6116b03
dcw2772 <dcw2772@g.rit.edu>
2021-02-22 19:17:34 -0500

    Created BulkUpdateSuggestionRepository.java. Identical to BulkCreateSuggestionRepository except uses BulkUpdateMonumentSuggestions instead of BulkCreateMonumentSuggestions

---
+++ src/main/java/com/monumental/repositories/suggestions/BulkUpdateSuggestionRepository.java
@@ -0,0 +1,22 @@
+package com.monumental.repositories.suggestions;
+
+import com.monumental.models.User;
+import com.monumental.models.suggestions.BulkUpdateMonumentSuggestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+@Transactional
+public interface BulkUpdateSuggestionRepository extends JpaRepository<BulkUpdateMonumentSuggestion, Integer>{
+
+    /**
+     * Get all BulkUpdateMonumentSuggestions created by the specified updatedBy
+     * @param updatedBy - User object to get all of the BulkUpdateMonumentSuggestions that were created by it
+     * @return List<BulkUpdateMonumentSuggestions> - List of BulkUpdateMonumentSuggestions created by the specified
+     * updatedBy
+     */
+    List<BulkUpdateMonumentSuggestion> getAllByUpdatedBy(User updatedBy);
+}
