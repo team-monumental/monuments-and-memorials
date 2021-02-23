97ba34916e73e49f133fb31f32f3b0013c4b2f34
dcw2772 <dcw2772@g.rit.edu>
2021-02-23 13:54:41 -0500

    Created a BulkUpdateMonumentRequest.java class.

---
+++ src/main/java/com/monumental/controllers/helpers/BulkUpdateMonumentRequest.java
@@ -0,0 +1,73 @@
+package com.monumental.controllers.helpers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.monumental.services.MonumentService;
+import com.monumental.util.csvparsing.ZipFileHelper;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.zip.ZipFile;
+
+public class BulkUpdateMonumentRequest {
+
+    private MultipartFile zip;
+    private MultipartFile csv;
+    private MultipartFile mapping;
+
+    public MultipartFile getZip() {
+        return this.zip;
+    }
+
+    public void setZip(MultipartFile zip) {
+        this.zip = zip;
+    }
+
+    public MultipartFile getCsv() {
+        return this.csv;
+    }
+
+    public void setCsv(MultipartFile csv) {
+        this.csv = csv;
+    }
+
+    public MultipartFile getMapping() {
+        return this.mapping;
+    }
+
+    public void setMapping(MultipartFile mapping) {
+        this.mapping = mapping;
+    }
+
+    public static class ParseResult {
+        public String csvFileName;
+        public List<String[]> csvContents;
+        public Map<String, String> mapping;
+        public ZipFile zipFile;
+    }
+
+    @SuppressWarnings("unchecked")
+    public BulkCreateMonumentRequest.ParseResult parse(MonumentService monumentService) throws IOException {
+        String json = new String(this.getMapping().getBytes());
+        ObjectMapper mapper = new ObjectMapper();
+
+        BulkCreateMonumentRequest.ParseResult result = new BulkCreateMonumentRequest.ParseResult();
+
+        result.mapping = mapper.readValue(json, Map.class);
+
+        if (this.getZip() != null) {
+            result.zipFile = ZipFileHelper.convertMultipartFileToZipFile(this.getZip());
+            result.csvContents = monumentService.readCSVFromZip(result.zipFile);
+        } else {
+            result.csvContents = monumentService.readCSV(this.getCsv());
+        }
+
+        if (this.getCsv() != null) {
+            result.csvFileName = this.getCsv().getOriginalFilename();
+        }
+
+        return result;
+    }
+
+}
