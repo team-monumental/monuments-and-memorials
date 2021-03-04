34305322cece4ccef90cfd6ce8778376e5f3d217
dcw2772 <dcw2772@g.rit.edu>
2021-03-04 14:09:19 -0500

    Finished adding bulk update functionality

--- client/src/pages/AdminPage/MonumentBulkUpdatePage/MonumentBulkUpdatePage.js
+++ client/src/pages/AdminPage/MonumentBulkUpdatePage/MonumentBulkUpdatePage.js
@@ -46,4 +46,86 @@ class MonumentBulkUpdatePage extends React.Component {
             ...state.bulkUpdatePage
         };
     }
+
+    handleValidationSubmit(form) {
+        const { dispatch } = this.props;
+
+        // Send the .zip or .csv file and the mapping to the server to be processed
+        dispatch(bulkValidateSuggestions(form));
+        this.setState({showValidationResults: true});
+    }
+
+    handleCreateSubmit(form) {
+        const { dispatch, user } = this.props;
+
+        // Make the appropriate API call
+        // Researchers and Admins bypass Suggestions and can directly bulk-create new Monuments
+        if (user && Role.RESEARCHER_OR_ABOVE.includes(user.role.toUpperCase())) {
+            dispatch(bulkUpdateMonuments(form));
+        }
+        // Any other role has to create a Suggestion
+        else {
+            dispatch(bulkCreateSuggestions(form));
+        }
+
+        this.setState({showValidationResults: false});
+    }
+
+    handleErrorModalClose() {
+        this.setState({showingErrorModal: false});
+    }
+
+    render() {
+        let { showingErrorModal, showValidationResults, term, showCreateResults } = this.state;
+        const { bulkSuggestionCreatePending, bulkSuggestionValidatePending, bulkCreatePending, validationResult,
+            validationError, createSuggestionResult, createSuggestionError, createSuggestionProgress,
+            createProgress, createResult, createError } = this.props;
+
+        showCreateResults = showCreateResults && createResult
+        showValidationResults = showValidationResults && !bulkSuggestionValidatePending && !showCreateResults;
+
+        return (
+            <div className="bulk-update page d-flex justify-content-center">
+                <Helmet title={`Bulk ${term} | Monuments and Memorials`}/>
+                <Spinner show={bulkSuggestionValidatePending}/>
+                <BulkUpdateForm
+                    onValidationSubmit={(form) => this.handleValidationSubmit(form)}
+                    onCreateSubmit={(form) => this.handleCreateSubmit(form)}
+                    onResetForm={() => this.setState({showValidationResults: false, showCreateResults: false})}
+                    term={term} pastTenseTerm={this.getTermPastTense()} actionHappeningTerm={this.getTermActionHappening()}
+                    {...{validationResult, createSuggestionResult, showValidationResults, showCreateResults, createResult}}
+                />
+                <Modal show={bulkSuggestionCreatePending || bulkCreatePending}>
+                    <Modal.Header className="pb-0">
+                        <Modal.Title>
+                            Bulk {this.getTermActionHappening()} Monuments or Memorials
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div className="mb-2">
+                            Please wait while your monuments or memorials are {this.getTermPastTense().toLowerCase()}...
+                        </div>
+                        <ProgressBar now={(createSuggestionProgress || createProgress) * 100}/>
+                    </Modal.Body>
+                </Modal>
+                <ErrorModal
+                    showing={showingErrorModal}
+                    errorMessage={(validationError || createSuggestionError || createError || {}).message || ''}
+                    onClose={() => this.handleErrorModalClose()}
+                />
+            </div>
+        );
+    }
+
+    getTermPastTense() {
+        const { term } = this.state;
+        return term === 'Suggest' ? term + 'ed' : term + 'd';
+    }
+
+    getTermActionHappening() {
+        const { term } = this.state;
+        return term === 'Suggest' ? term + 'ing' : 'Creating';
+    }
 }
+
+export default withRouter(connect(MonumentBulkUpdatePage.mapStateToProps(MonumentBulkUpdatePage)))
