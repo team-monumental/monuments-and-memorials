9f89c4282c63a345f14048e272fcd17b0fc53075
dcw2772 <dcw2772@g.rit.edu>
2021-03-11 09:36:43 -0500

    Replaced old constants with new ones

--- client/src/reducers/bulk-update.js
+++ client/src/reducers/bulk-update.js
@@ -1,10 +1,10 @@
 import {
-    CREATE_BULK_SUGGESTION_PENDING,
-    CREATE_BULK_SUGGESTION_SUCCESS,
-    CREATE_BULK_SUGGESTION_ERROR,
-    VALIDATE_BULK_SUGGESTION_PENDING,
-    VALIDATE_BULK_SUGGESTION_SUCCESS,
-    VALIDATE_BULK_SUGGESTION_ERROR,
+    CREATE_BULK_UPDATE_SUGGESTION_PENDING,
+    CREATE_BULK_UPDATE_SUGGESTION_SUCCESS,
+    CREATE_BULK_UPDATE_SUGGESTION_ERROR,
+    VALIDATE_BULK_UPDATE_SUGGESTION_PENDING,
+    VALIDATE_BULK_UPDATE_SUGGESTION_SUCCESS,
+    VALIDATE_BULK_UPDATE_SUGGESTION_ERROR,
     BULK_UPDATE_MONUMENTS_PENDING,
     BULK_UPDATE_MONUMENTS_SUCCESS,
     BULK_UPDATE_MONUMENTS_ERROR
@@ -12,8 +12,8 @@ import {
 import { LOCATION_CHANGE } from 'connected-react-router';
 
 const initialState = {
-    bulkSuggestionCreatePending: false,
-    bulkSuggestionValidatePending: false,
+    bulkUpdateSuggestionCreatePending: false,
+    bulkUpdateSuggestionValidatePending: false,
     bulkUpdatePending: false,
     result: {},
     error: null
@@ -24,39 +24,39 @@ export default function bulkUpdatePage(state = initialState, action) {
     switch (action.type) {
         case LOCATION_CHANGE:
             return initialState;
-        case CREATE_BULK_SUGGESTION_PENDING:
+        case CREATE_BULK_UPDATE_SUGGESTION_PENDING:
             return {
                 ...state,
-                bulkSuggestionCreatePending: true,
+                bulkUpdateSuggestionCreatePending: true,
                 createSuggestionProgress: action.progress
             };
-        case CREATE_BULK_SUGGESTION_SUCCESS:
+        case CREATE_BULK_UPDATE_SUGGESTION_SUCCESS:
             return {
                 ...state,
-                bulkSuggestionCreatePending: false,
+                bulkUpdateSuggestionCreatePending: false,
                 createSuggestionResult: action.payload
             };
-        case CREATE_BULK_SUGGESTION_ERROR:
+        case CREATE_BULK_UPDATE_SUGGESTION_ERROR:
             return {
                 ...state,
-                bulkSuggestionCreatePending: false,
+                bulkUpdateSuggestionCreatePending: false,
                 createSuggestionError: action.error
             };
-        case VALIDATE_BULK_SUGGESTION_PENDING:
+        case VALIDATE_BULK_UPDATE_SUGGESTION_PENDING:
             return {
                 ...state,
-                bulkSuggestionValidatePending: true
+                bulkUpdateSuggestionValidatePending: true
             };
-        case VALIDATE_BULK_SUGGESTION_SUCCESS:
+        case VALIDATE_BULK_UPDATE_SUGGESTION_SUCCESS:
             return {
                 ...state,
-                bulkSuggestionValidatePending: false,
+                bulkUpdateSuggestionValidatePending: false,
                 validationResult: action.payload
             };
-        case VALIDATE_BULK_SUGGESTION_ERROR:
+        case VALIDATE_BULK_UPDATE_SUGGESTION_ERROR:
             return {
                 ...state,
-                bulkSuggestionValidatePending: false,
+                bulkUpdateSuggestionValidatePending: false,
                 validationError: action.error
             };
         case BULK_UPDATE_MONUMENTS_PENDING:
