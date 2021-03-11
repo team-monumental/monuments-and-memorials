8dc00684b14f1644d2e6d3ef9bdb04df90742c0c
sxm7571 <32307459+sxm7571@users.noreply.github.com>
2021-03-11 16:59:29 -0500

    Decade bug (#173)
    * Names added to about page.
    
    * Decade story.
    
    * Decade bug fix.
    
    * Added extra years.

--- client/src/components/Search/Filters/FilterTypes/DateFilter/DateFilter.js
+++ client/src/components/Search/Filters/FilterTypes/DateFilter/DateFilter.js
@@ -134,9 +134,24 @@ export default class DateFilter extends React.Component {
                 <span className="mr-2">Monuments or memorials created in the</span>
                 <Form.Control as="select" className="min-width-select" onChange={event => this.handleDateFilter(Mode.DECADE, event.target.value)} value={decade}>
                     <option value="null">None</option>
-                    {decades.map(decade => (
-                        <option value={decade} key={decade}>{decade}s</option>
-                    ))}
+                    <option value="1850">1850s</option>
+                    <option value="1860">1860s</option>
+                    <option value="1870">1870s</option>
+                    <option value="1880">1880s</option>
+                    <option value="1890">1890s</option>
+                    <option value="1900">1900s</option>
+                    <option value="1910">1910s</option>
+                    <option value="1920">1920s</option>
+                    <option value="1930">1930s</option>
+                    <option value="1940">1940s</option>
+                    <option value="1950">1950s</option>
+                    <option value="1960">1960s</option>
+                    <option value="1970">1970s</option>
+                    <option value="1980">1980s</option>
+                    <option value="1990">1990s</option>
+                    <option value="2000">2000s</option>
+                    <option value="2010">2010s</option>
+                    <option value="2020">2020s</option>
                 </Form.Control>
             </div>
         )
