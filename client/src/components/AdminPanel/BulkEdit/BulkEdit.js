f83834450faba8c9155dd1c8e4e3e8dc0fb5bb2f
Tim <tdm4018@rit.edu>
2021-11-09 16:56:52 -0500

    Bulk edit card and default search bar

---
+++ client/src/components/AdminPanel/BulkEdit/BulkEdit.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import './BulkEdit.scss';
+import { withRouter } from 'react-router-dom';
+import { Button, Card } from "react-bootstrap";
+import SearchPage from "../../../pages/SearchPage/SearchPage";
+
+
+class BulkEdit extends React.Component {
+    render() {
+        const { mode, history } = this.props;
+
+        return (
+            <div className="bulk-edit">
+                <Card style={{maxWidth: mode === 'search' ? '800px' : ''}}>
+                    <Card.Header className="d-flex justify-content-between align-items-center">
+                        <Card.Title>
+                            Bulk Edit
+                        </Card.Title>
+                        {mode === 'monument' && <>
+                            <Button variant="light" className="h-100" onClick={() => history.goBack()}>Back</Button>
+                        </>}
+                    </Card.Header>
+                    <Card.Body>
+                        {(!mode || mode === 'search') && <>
+                            <SearchPage searchUri="/panel/manage/monuments/search" monumentUri="/panel/manage/monuments/monument"
+                                        hideMap/>
+                        </>}
+                    </Card.Body>
+                </Card>
+            </div>
+        );
+    }
+}
+
+export default withRouter(BulkEdit);
--- client/src/pages/AdminPage/BulkEditPage/BulkEditPage.js
+++ client/src/pages/AdminPage/BulkEditPage/BulkEditPage.js
@@ -2,13 +2,16 @@ import * as React from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
+import BulkEdit from "../../../components/AdminPanel/BulkEdit/BulkEdit";
 
 class BulkEditPage extends React.Component {
 
 
     render() {
+        const { mode } = this.props;
         return (<>
             <Helmet title={`Bulk Edit | Monuments and Memorials`}/>
+            <BulkEdit mode={mode}/>
         </>);
     }
 }
