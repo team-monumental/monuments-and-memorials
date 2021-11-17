7441c0ea28bbd770f3703e412e32c5d73db232ed
Henry Larson <hxl1116@rit.edu>
2021-11-17 00:31:20 -0500

    Fixed layout so search, queue panels are side-by-side; removed `max-width` property so cards will take up available space; TODO's updated

--- client/src/components/AdminPanel/BulkEdit/BulkEditPanel.js
+++ client/src/components/AdminPanel/BulkEdit/BulkEditPanel.js
@@ -1,22 +1,44 @@
 import React from 'react'
-import {Card, Col, Container, Row} from "react-bootstrap";
+import {Card, CardGroup, Col, Container, Row} from "react-bootstrap";
 import BulkEditSearchPanel from "./BulkEditSearchPanel";
 
 import './BulkEdit.scss'
+import BulkEditUpdateForm from "./Queue/BulkEditUpdateForm";
 
 const BulkEditPanel = (props) => {
     return (
         <div className="bulk-edit">
-            <Card>
-                <Card.Header>
-                    <Card.Title>
-                        Bulk Edit Monuments and Memorials
-                    </Card.Title>
-                </Card.Header>
-                <Card.Body>
-                    <BulkEditSearchPanel/>
-                </Card.Body>
-            </Card>
+            <Container>
+                <Row>
+                    <Col lg={6}>
+                        <Card>
+                            <Card.Header>
+                                <Card.Title>
+                                    Bulk Edit Monuments and Memorials
+                                </Card.Title>
+                            </Card.Header>
+                            <Card.Body>
+                                <BulkEditSearchPanel/>
+                            </Card.Body>
+                        </Card>
+                        {/* TODO: Search panel buttons here */}
+                    </Col>
+                    <Col lg={6}>
+                        <Card>
+                            <Card.Header>
+                                <Card.Title>
+                                    Editing Queue
+                                </Card.Title>
+                            </Card.Header>
+                            <Card.Body>
+                                {/* TODO: Will need to create a new component with less bloat */}
+                                <BulkEditUpdateForm/>
+                            </Card.Body>
+                        </Card>
+                        {/* TODO: Queue panel buttons here */}
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
