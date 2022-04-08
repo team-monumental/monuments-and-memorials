bab21ba591fd07e18d8e3181e4a54969451a0cd8
Henry Larson <hxl1116@rit.edu>
2022-04-08 14:21:57 -0400

    Implemented inserting new refs

--- client/src/components/AdminPanel/BulkEdit/Queue/QueueItemRefs.js
+++ client/src/components/AdminPanel/BulkEdit/Queue/QueueItemRefs.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import {Field} from "formik";
 import QueueItemRef from "./QueueItemRef";
 import {Col, Form} from "react-bootstrap";
 
 
 const QueueItemRefs = ({form: {values}, remove, push, ...props}) => {
+    const [newRefId, setNewRefId] = useState(-1)
+
+    const handlePush = () => {
+        push({
+            id: newRefId,
+            createdDate: new Date(),
+            lastModifiedDate: null,
+            createdBy: null,
+            url: ''
+        })
+
+        setNewRefId(newRefId - 1)
+    }
+
     return (
         <Form.Row>
             <Form.Group as={Col}>
@@ -15,6 +29,7 @@ const QueueItemRefs = ({form: {values}, remove, push, ...props}) => {
                            type="text"
                            component={QueueItemRef}/>
                 ))}
+                <Form.Text><a href="#" onClick={handlePush}>add reference</a></Form.Text>
             </Form.Group>
         </Form.Row>
     )
