97fa0780f61e7e9b58e71b7ade079fa6e1864672
Daniel Nelson <43452004+dn3531@users.noreply.github.com>
2022-11-01 17:14:44 -0400

    Cmm 44 improve pagination (#228)
    * fix bug with infinite pagination
    
    * use math ciel to show more pages

--- client/src/components/AdminPanel/BulkEdit/Search/SearchResultNav.js
+++ client/src/components/AdminPanel/BulkEdit/Search/SearchResultNav.js
@@ -19,7 +19,7 @@ const SearchResultNav = ({results, setItems, setActive, active, setStep, step}) 
         <Pagination>
             <Pagination.First disabled={active === 0} onClick={() => handleActive(0)}/>
             <Pagination.Prev disabled={active === 0} onClick={() => handleActive(active - 1)}/>
-            {Array.from({length: results.length / step}, (x, i) => i).map(idx =>
+            {Array.from({length: Math.ceil(results.length / step)}, (x, i) => i).map(idx =>
                 <Pagination.Item key={`page-item-${idx}`} active={active === idx} onClick={() => setActive(idx)}>
                     {idx + 1}
                 </Pagination.Item>
